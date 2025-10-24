07b809515a43b4b6b62ceeed9bfbd446cc4407c2
satr <satr@cowi.com>
2025-10-24 14:22:25 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,13 @@ ECF_soil = st.number_input("Soil ECF")
 h = np.linspace(0,2*height,101)
 carb = carbon_calculation.calc_carbon_per_FA(h,path_width,slope_grad,ECF_soil)
 
-df = pd.DataFrame({'x':h,'y':carb[:,0]})
+df = pd.DataFrame({'h':h,
+                   'Unreinforced':carb[:,0],
+                   'geo_low':carb[:,1],
+                   'geo_mid':carb[:,2],
+                   'geo_high':carb[:,3]})
 
-alt_chart = (alt.Chart(df).mark_line().encode(x='x',y='y'))
+alt_chart = (alt.Chart(df).mark_line().encode(x='h',y='Unreinforced') + 
+             alt.Chart(df).mark_line().encode(x='h',y='geo_mid') +
+             alt.Chart(df).mark_area().encode(x='h',y='geo_low',y2='geo_high'))
 st.altair_chart(alt_chart)
